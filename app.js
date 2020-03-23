33bed965332bdb90e749277e7dc2d503cd17234b
Vendy Prum <vendy@thinkful.com>
2020-03-23 16:37:38 -0700

    third drill completed

--- app.js
+++ app.js
@@ -18,12 +18,36 @@ app.get('/cipher', (req,res)=>{
   const shift = Number(req.query.shift);
   let charcode = 0;
   let result = '';
-  console.log(req.query.text, req.query.shift);
   for(let i = 0; i < text.length; i++){
     charcode = (text[i].charCodeAt() + shift);
     result += String.fromCharCode(charcode);
   }
   res.send('this is the result: ' + result);
+});
+
+//third drill
+app.get('/lotto', (req,res)=>{
+  const numbers =req.query.numbers.map(number => Number(number));
+  let newArray = [];
+  let randomArray = [];
+  for(let i = 0; i < 6;i++){
+    randomArray.push(Math.floor(Math.random()*20 +1));
+  }
+  for(let i = 0; i < numbers.length; i++){
+    if(randomArray.includes(numbers[i])){
+      newArray.push(numbers[i]);
+    }
+  }
+  console.log('user array' +numbers);
+  console.log('random array' + randomArray);
+  console.log('matching array :' + newArray);
+  if(newArray.length === 6){
+    res.send('Congratulations! You win $100!');
+  } else if(newArray.length >= 5){
+    res.send('Congratulations, you win a free ticket');
+  } else{
+    res.send('Sorry, you lose');
+  }
 });
 
 
